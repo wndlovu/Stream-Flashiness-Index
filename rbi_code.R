58f652792cac4daf992340fa368985e3ca0b6e62
wndlovu <71508257+wndlovu@users.noreply.github.com>
2021-06-01 14:40:26 -0400

    Add files via upload
    converted ft3/s to mm/day

---
+++ rbi_code.R
@@ -0,0 +1,51 @@
+
+install.packages("dataRetrieval")
+
+library(ggplot2)
+library(tidyverse)
+library(dataRetrieval)
+
+# USGS Gage #
+siteNumber <- "01076500" # Pemigewasset R - Hubbard Brook
+
+parameterCd <- "00060"  # Discharge
+startDate <- "2009-10-01"  
+endDate <- "2010-09-30"
+
+
+# Q is in CFS
+q2010 <- readNWISdv(siteNumber,parameterCd,startDate, endDate)
+
+# Convert to mm/day
+
+# To do this, you're going to need to use a function to get the drainage area of this site
+# https://cran.r-project.org/web/packages/dataRetrieval/vignettes/dataRetrieval.html
+# Find a function that will extract the drainage area for this site (mi2)
+
+
+# extract drain area from site data
+drainageArea <- readNWISsite(siteNumber) %>% 
+  select(drain_area_va) 
+
+
+# Extract the drainage area
+# Convert from cfs to mm/day
+
+q2010_2 <- q2010 %>% 
+  mutate(miles3_s = X_00060_00003/147197940448.88, # cfs to mi3/s
+         miles3_day = miles3_s/86400,              # mi3/s to mi3/day
+         miles_day = miles3_day/drainageArea$drain_area_va, #divide by area to get mi/day
+         mm_day = miles_day*(1609.344*1000))    
+
+
+qdiff <- 0
+for (i in 2:length(q2010$X_00060_00003)){
+  qdiff[i] <- q2010$X_00060_00003[i] - q2010$X_00060_00003[i-1]
+  # take the difference between qi and qi-1
+  # take the absolute value
+}
+
+rbi <- sum(abs(qdiff))/sum(q2010$X_00060_00003)
+
+
+
