61379420f3a55a0160f2247fb2f194bf36b1e7d9
Wayne Ndlovu <waynendlovu@Waynes-MacBook-Air.local>
2021-06-04 14:00:02 -0400

    - stored site data in lists - added function to calculate rbi - calculated the rbi for multiple sites using function and for loop

--- rbi_code.R
+++ rbi_code.R
@@ -7,36 +7,52 @@ library(dataRetrieval)
 library(lubridate)
 library(data.table)
 
-# USGS Gage # site in Maine
-siteNumber <- "01011000" # Pemigewasset R - Hubbard Brook
+# download discharge data for site 1 and 2 USGS Gage #
+siteNumber1 <- "01011000" # Pemigewasset R - Hubbard Brook
+siteNumber2 <- "01076500" # Pemigewasset R - Hubbard Brook
 
-parameterCd <- "00060"  # Discharge
-startDate <- "1970-10-01"   
-#endDate <- "2010-09-30"
-endDate <- ymd(startDate) + years(50) # download data for 50 more yrs
+parameterCd <- "00060"  # Discharge in cfs
+startDate <- "1970-10-01"  
+endDate <- "2020-09-30"
+#endDate <- ymd(startDate) + years(50) # download data for 50 more yrs
 
-# Q is in CFS
-q2010 <- readNWISdv(siteNumber,parameterCd,startDate, endDate)
 
-# Convert to mm/day
+# read discharge data and save
+q2010_s1 <- readNWISdv(siteNumber1,parameterCd,startDate, endDate)
+q2010_s2 <- readNWISdv(siteNumber2,parameterCd,startDate, endDate)
+
+# create a list of the discharge data
+list_data = list(q2010_s1,q2010_s2)
+site <- list_data[[1]]
+
+# read site decription and save (will use to get the discharge area)
+drainageArea1 <- readNWISsite(siteNumber1)
+drainageArea2 <- readNWISsite(siteNumber2) 
+
+# create list of the site description
+list_drainage = list(drainageArea1, drainageArea2) 
+
+# get the drainage area for site 1 in the list
+drainageArea <- list_drainage[[1]] %>% 
+  filter(site_no %in% site$site_no) %>%  
+  select(site_no, drain_area_va) 
+
 
 # To do this, you're going to need to use a function to get the drainage area of this site
 # https://cran.r-project.org/web/packages/dataRetrieval/vignettes/dataRetrieval.html
 # Find a function that will extract the drainage area for this site (mi2)
 
 
-# extract drain area from site data
-drainageArea <- readNWISsite(siteNumber) %>% 
-  select(drain_area_va) 
-
-
 # Extract the drainage area
 # Convert from cfs to mm/day
 
-q2010_2 <- q2010 %>% 
+
+# supposed to calculate discharge using the area for each site, so join the dataframe with drainageArea
+q2010_2 <- site %>% 
+  left_join(drainageArea) %>% 
   mutate(miles3_s = X_00060_00003/147197940448.88, # cfs to mi3/s
          miles3_day = miles3_s*86400,              # mi3/s to mi3/day
-         miles_day = miles3_day/drainageArea$drain_area_va, #divide by area to get mi/day
+         miles_day = miles3_day/drain_area_va, #divide by area to get mi/day
          mm_day = miles_day*(1609.344*1000))    
 
 
@@ -59,44 +75,31 @@ qdiff <- 0    # initialise discharge difference between 2 days
 year_num <- 0 # initialise year counter
 rbi_values <- 0 # unitialise calculated rbi
 
-site_num <- 0
 
-
-
-for (t in unique(q2010_2$waterYear)){ # loop through the waterYear and select all unique values
-  year_num <- year_num + 1 # first unique waterYear is counted as year 1
-  year2 <- filter(q2010_2, waterYear == t) # filter for all unique waterYears
-  for (s in unique(year2$site_no)){ # trying to loop over each site 
-    site_num <- site_num + 1
-    sites <- filter(year2, site_no == s)
-    for (i in 2:length(year2$X_00060_00003)){ # offset discharge values by 1
-      qdiff[i] <- year2$mm_day[i] - year2$mm_day[i-1] # take the difference between qi and qi-1
-    }
+# Create RBI function
+Q1 = q2010_2$mm_day
+rbi <- function(Q){
+  qdiff <- 0 # initialise discharge difference between 2 days
+  for (i in 2:length(Q)){ # offset discharge values by 1
+    qdiff[i] <- Q[i] - Q[i-1] # take the difference between qi and qi-1
   }
-  rbi_values[year_num] <- (sum(abs(qdiff))/sum(year2$mm_day)) # and calculate rbi for each year number
+  rbi_values <- (sum(abs(qdiff))/sum(Q)) # and calculate rbi for each year number  
+  return(rbi_values)
 }
+s <- rbi(Q=Q1)
 
+rbi_values_df <- data.frame() # create df to save calculate rbi values
+rbi_wy = data.frame(water_yr = unique(q2010_2$waterYear), rbiValue = " ")# create df and save the waterYear
+colnames(rbi_wy)[1] <- "waterYear" # rename waterYear column
 
-# create data frame with rbi values and rename the variable to rbi
-rbi_values_df <- as.data.frame(rbi_values)
-colnames(rbi_values_df) <- "rbi"
-#add new variable that counts the rows
-rbi_values_df <- rbi_values_df %>% 
-  mutate(observation = 1:n())
-
-# create data frame with the water years and rename variable to water year
-years_all_df <- as.data.frame(unique(q2010_2$waterYear))
-colnames(years_all_df) <- "water_Year"
-# add new variable that counts the rows
-years_all_df <- years_all_df %>% 
-  mutate(observation = 1:n())
-
-#create water year and rbi dataframe by joining the dataframes
-wy_rbi <- years_all_df %>% 
-  left_join(rbi_values_df) %>% 
-  select(observation, water_Year, rbi)
-
-
+# loop that calculates rbi for all the waterYears
+for (t in unique(q2010_2$waterYear)){ # get all the waterYears 
+  year2 <- filter(q2010_2, waterYear == t)
+  rbi_val <- rbi(Q = year2$mm_day) # call the function and calculate rbi
+  calc_rbi <- data.frame(rbi_val) # add each calculated rbi to calc_rbi dataframe
+  rbi_values_df <- rbind(rbi_values_df,calc_rbi) # append the calc_rbi to rbi_values df
+}
+rbi_wy$rbiValue <- rbi_values_df$rbi_val # add the rbi values to the rbi_wy
 
 
 
