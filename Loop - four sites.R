24b9073446fc35efa67fc1f8b86afbfb83e89d9d
Wayne Ndlovu <waynendlovu@Waynes-MacBook-Air.local>
2021-06-08 10:39:21 -0400

    loop that stores data for 4 different sites in a list

--- Loop - four sites.R
+++ Loop - four sites.R
@@ -6,21 +6,22 @@ library(dataRetrieval)
 library(lubridate)
 
 
-1011000
-1013500
-1017000
-1019000
+01011000
+01013500
+01017000
+01019000
+
 
 sites_data_list <- list() #create an empty list to add the site data into
-site_nums <- list("1011000", "1013500", "1017000", "1019000") # list of site numbers
+site_nums <- c("01011000", "01013500", "01017000", "01019000") # list of site numbers
 
 for (i in 1:length(site_nums)){
-  parameterCd <- "00060" # dicharge parameter
   startDate <- "1970-10-01"  
   endDate <- "2020-09-30"
-  site_name <- site_nums[[i]] # get site i and get the site data
-  site_data <- readNWISdv(site_name, parameterCd,startDate, endDate)# not working
-  sites_data_list <- append(sites_data_list, site_data) # append the site data to the site data list
+  parameterCd <- "00060" # dicharge parameter
+  site_name <- site_nums[i] # get site i and get the site data
+  site_data <- readNWISdv(site_name, parameterCd,startDate, endDate) # get the site data
+  sites_data_list[[i]] <- site_data # append the site data to the site data list
 }
 
 
