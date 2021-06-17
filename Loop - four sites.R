b923c93a7b7672384a35e6f4d0a188acad093a62
Wayne Ndlovu <waynendlovu@Waynes-MacBook-Air.local>
2021-06-17 15:07:10 -0400

    uploading file with rbi values for all sites

--- Loop - four sites.R
+++ Loop - four sites.R
@@ -4,13 +4,14 @@ library(ggplot2)
 library(tidyverse)
 library(dataRetrieval)
 library(lubridate)
-
+library (plyr)
+library(purrr)
 
 site_nums <- read_csv("nesites.csv") %>% # read sites list into R
   mutate(STAID = as.character(paste0("0", STAID))) # add a 0 to start of all sites
 
-
 site_nums <- as.character(site_nums$STAID) # re-save data as array
+
 
 # create a dataframe with the area for each site
 drainageArea_df <- data.frame(readNWISsite(site_nums)) %>% 
@@ -31,6 +32,7 @@ for (i in 1:length(site_nums)){
            mm_day = miles_day*(1609.344*1000))
   sites_data_list[[i]] <- site_data # append the site data to the site data list
 }
+
 
 # add the waterYear variable
 for (site in 1:length(sites_data_list)){ # for every site in the site list
@@ -67,7 +69,8 @@ for (t in 1:length(sites_data_list)){ # for every site in the sites data list
   rbi_wy <- data.frame(matrix(ncol = 2, nrow = length(unique(sites_data_list[[t]]$waterYear)))) # create data frame with 2 columns
   rbi_wy$X1 =  unique(sites_data_list[[t]]$waterYear)# make unique waterYear the first variable
   colnames(rbi_wy)[1] <- "waterYear" # rename 1st variable to waterYear
-  colnames(rbi_wy)[2] <- "rbiValue" # rename 2nd variable to rbiValue
+  #colnames(rbi_wy)[2] <-  as.character(paste0("rbiValue_", sites_data_list[[t]]$site_no[1])) # rename 2nd variable to rbiValue and add site_no
+  colnames(rbi_wy)[2] <-  as.character(paste0(sites_data_list[[t]]$site_no[1])) # rename 2nd variable to rbiValue and add site_no
   rbi_wy_list[[t]] <- rbi_wy # add the rbi_wy dataframes for each year to the rbi_wy_list 
   
   rbi_values_df <- data.frame() # create an empty df for rbi values
@@ -79,6 +82,13 @@ for (t in 1:length(sites_data_list)){ # for every site in the sites data list
     rbi_values_df <- rbind(rbi_values_df,calc_rbi) # append the calc_rbi to rbi_values df
     #rbi_wy_list[[t]] <- rbi_values_df
   }
-  rbi_wy$rbiValue <- rbi_values_df$rbi_val # add rbi values to the rbiValue in rbi_wy 
+  rbi_wy[,2] <- rbi_values_df$rbi_val # add rbi values to the rbiValue in rbi_wy 
   rbi_wy_list[[t]] <- rbi_wy # add the new rbi_wy dataframes with rbi and WY to the rbi_wy_list 
 }
+
+
+rbiWy_dfAll <- do.call("cbind", rbi_wy_list) # create one dataframe from the rbi_wy_list 
+rbiWy_dfAll <- rbiWy_dfAll[!duplicated(as.list(rbiWy_dfAll))] # remove the duplicated waterYears
+
+
+# rbiWy_dfAll file is downloaded and saved as a .csv file
