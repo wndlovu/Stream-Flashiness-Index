3b282ffba8e0445290e9df5798dafe031cabfcc2
Wayne Ndlovu <waynendlovu@Waynes-MacBook-Air.local>
2021-06-22 13:52:37 -0400

    adding all results files

--- results/trend_analysis_results.R
+++ results/trend_analysis_results.R
@@ -1,11 +1,29 @@
 library(readr)
+library(dplyr)
 library(tidyverse)
 library(lubridate)
+library(janitor)
+library(data.table)
 
 # create trend analysis df with the important variables
 trend_analysis_df <- read_csv("mannKendall_analysis_df.csv") %>% 
   left_join(read_csv("sensSlope_analysis_df.csv")) %>% 
   select(site_no, tau, estimates,  S, sl, p.value)
 
-# export df as .csv
-write_csv(trend_analysis_df, "trend_analysis_df.csv")
+# export df as .csv (file already provided)
+#write_csv(trend_analysis_df, "trend_analysis_df.csv")
+
+#Other part of results to export into a CSV: RBI value
+#for each site, 1970, 2020
+rbiWy_1971_2020 <- read_csv("Trend_Analysis_Code/rbiWy_dfAll.csv") %>% 
+  filter(waterYear == c(1971, 2020))
+
+
+rbiWy_1971_2020 <- data.frame(t(rbiWy_1971_2020)) 
+rbiWy_1971_2020 <- setDT(rbiWy_1971_2020, keep.rownames = TRUE)[] %>% 
+  row_to_names(row_number = 1) %>% 
+  dplyr::rename(site_no = waterYear,
+                rbiValue_1970 = '1971',
+                rbiValue_2020 = '2020')
+  
+#write_csv(rbiWy_1971_2020, "rbiWy_1971_2020.csv")
