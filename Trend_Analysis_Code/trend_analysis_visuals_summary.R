56f60b242cbfaa51f400e81601b8b9b10ba8156f
Wayne Ndlovu <waynendlovu@Waynes-Air.home>
2021-06-30 11:47:40 -0400

    edited comments

--- Trend_Analysis_Code/trend_analysis_visuals_summary.R
+++ Trend_Analysis_Code/trend_analysis_visuals_summary.R
@@ -16,6 +16,7 @@ trend_analysis_df <- read_csv("results/mannKendall_analysis_df.csv") %>%
   left_join(read_csv("results/sensSlope_analysis_df.csv")) %>% 
   select(site_no, tau, estimates,  S, sl, p.value)
 
+
 # export df as .csv (file already provided)
 #write_csv(trend_analysis_df, "trend_analysis_df.csv")
 
@@ -23,6 +24,8 @@ trend_analysis_df <- read_csv("results/mannKendall_analysis_df.csv") %>%
 #for each site -> 1971 & 2020
 rbiWy_1971_2020 <- read_csv("Trend_Analysis_Code/rbiWy_dfAll.csv") %>% 
   filter(waterYear == c(1971, 2020))
+rbiWy_1971_2020
+
 
 # transform df from series and include site_no and rbi values for 1971 and 2020
 rbiWy_1971_2020 <- data.frame(t(rbiWy_1971_2020))  
@@ -32,12 +35,17 @@ rbiWy_1971_2020 <-  setDT(rbiWy_1971_2020, keep.rownames = TRUE)[] %>%
   dplyr::rename(site_no = waterYear,
                 rbiValue_1970 = '1971',
                 rbiValue_2020 = '2020')
+rbiWy_1971_2020
+
+
   
 # join trend analysis df with the rbi data
 plot_data <- rbiWy_1971_2020 %>% 
   left_join(trend_analysis_df) %>% 
   mutate(binary_pvalue = as.factor(ifelse(p.value < 0.05, 'significant', 'non-significant')), # when p.value is <0.05, binary_pvalue = significant
          slope_greater0 = as.factor(ifelse(estimates > 0, 'true', 'false'))) # when slope is greater than 0, slope_greater0 = true
+plot_data
+
 
 # create color blind palette
 safe_colorblind_palette <- c("#88CCEE",  "grey", "navy", "#117733", "#999933", "#AA4499", "black", 
@@ -80,11 +88,11 @@ plot1971 + plot2020 +  coord_cartesian(clip = "off") +
 
         
         
-# remove outlier in the data to see the trends at the top and lot results
+# remove outlier in the data to see the trends at the top and plot results
 plot_without_outlier <- plot_data %>% 
   filter(estimates > -0.02)
 
-plot1971_2 <- ggplot(plot_without_outlier, aes(x = rbiValue_1970, y = estimates, color = binary_pvalue))+
+plot1971_noOutlier <- ggplot(plot_without_outlier, aes(x = rbiValue_1970, y = estimates, color = binary_pvalue))+
   geom_point()+
   labs(x = "RBI 1971", 
        y = "Theil sen slope")+
@@ -97,7 +105,7 @@ plot1971_2 <- ggplot(plot_without_outlier, aes(x = rbiValue_1970, y = estimates,
     axis.text = element_text(size = 9)
   )
 
-plot2020_2 <- ggplot(plot_without_outlier, aes(x = rbiValue_2020, y = estimates, color = binary_pvalue))+
+plot2020_noOutlier <- ggplot(plot_without_outlier, aes(x = rbiValue_2020, y = estimates, color = binary_pvalue))+
   geom_point()+
   labs(x = "RBI 2020")+
   scale_color_manual(legend_title, values = safe_colorblind_palette, labels = c("non-significant p value", "significant p value"))+
@@ -109,7 +117,7 @@ plot2020_2 <- ggplot(plot_without_outlier, aes(x = rbiValue_2020, y = estimates,
     legend.text = element_text(size = 9),
   )
 
-plot1971_2 + plot2020_2 +  coord_cartesian(clip = "off") +
+plot1971_noOutlier + plot2020_noOutlier +  coord_cartesian(clip = "off") +
   theme(plot.margin = margin(t = 10, r = 10, b = 40, l = 10),
         plot.tag.position = c(0.2, -0.1))
 ## end of code with removed outlier
